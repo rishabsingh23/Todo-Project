9c9d6b9379f7a7688d5a99bd003ef92f9f3c1587
rishabsingh23 <rishabhsingh2219@gmail.com>
2024-04-08 10:15:09 +0530

    Initial commit

---
+++ app/Services/TodoService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Todo;
+use Illuminate\Support\Facades\Lang;
+
+class TodoService
+{
+    public function getAllTodos()
+    {
+        $todos = Todo::orderBy('created_at', 'desc')->get();
+    
+        if ($todos->isEmpty()) {
+            return "Todo list not found";
+        }
+    
+        return $todos;
+    }
+    
+    public function createTodo(array $data)
+    {
+        $todo = Todo::create($data);
+        $message = Lang::get('messages.todo_created');
+        return ['message' => $message];
+    }
+
+    public function getTodoById(Todo $todo)
+    {
+        return $todo;
+    }
+
+    public function updateTodo(Todo $todo, array $data)
+    {
+        $todo->update($data);
+        $message = Lang::get('messages.todo_updated');
+        return ['message' => $message, 'todo' => $todo];
+    }
+
+    public function deleteTodo(Todo $todo)
+    {
+        $todo->delete();
+        $message = Lang::get('messages.todo_deleted');
+        return ['message' => $message];
+    }
+}
